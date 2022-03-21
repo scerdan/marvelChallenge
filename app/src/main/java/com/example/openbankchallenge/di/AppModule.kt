b61d66d9309be54af3660ea316f4f5646f8b78b3
Santiago Cerdán <santyjoell@gmail.com>
2022-03-21 09:36:07 -0300

    Add DI MODULE

---
+++ app/src/main/java/com/example/openbankchallenge/di/AppModule.kt
@@ -0,0 +1,24 @@
+package com.example.openbankchallenge.di
+
+import com.example.openbankchallenge.BuildConfig
+import com.example.openbankchallenge.data.ApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+    @Provides
+    @Singleton
+    fun provideRetrofitInstance(): ApiService =
+        Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiService::class.java)
+}
